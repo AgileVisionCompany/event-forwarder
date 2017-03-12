cba32a7e2503a55208b27c64a87bf491ca1966d4
Volodymyr Rudyi <volodymyr@agilevision.pl>
2017-03-12 21:59:53 +0200

    Add SNS Subscription Confirmation

--- index.js
+++ index.js
@@ -5,16 +5,29 @@ const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 const bodyParser = require('body-parser')
+const request = require('request')
 
+const overrideContentType = function(req, res, next){
+  if (req.headers['x-amz-sns-message-type']) {
+      req.headers['content-type'] = 'application/json;charset=UTF-8'
+  }
+  next()
+}
+
+app.use(overrideContentType)
 app.use(bodyParser.json())
 
 app.get('/', function(req, res,next) {
     res.send({'Health': 'OK'})
 });
 
-app.post('/forward-event', function(request, response){
-  io.sockets.emit('event', request.body)
-  response.send({'Success': true})
+app.post('/forward-event', function(req, res){
+  if (req.headers['x-amz-sns-message-type'] === 'SubscriptionConfirmation'){
+    request.get(req.body.SubscribeURL)
+  } else {
+    io.sockets.emit('event', req.body)
+  }
+  res.send({'Success': true})
 })
 
 server.listen(80)
